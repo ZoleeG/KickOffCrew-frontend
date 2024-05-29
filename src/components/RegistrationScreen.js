4a75e73b31bdacbdece51b4656c6bf5c83388ca3
Gurtaj <gurtajs98@gmail.com>
2024-05-29 16:48:14 +0100

    completed user registration form validation

--- src/components/RegistrationScreen.js
+++ src/components/RegistrationScreen.js
@@ -7,6 +7,7 @@ import {
   TextInput,
   ActivityIndicator,
   ScrollView,
+  StyleSheet
 } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -28,278 +29,329 @@ function RegistrationScreen({ navigation }) {
   const [last_name, setLast_name] = useState("");
   const [avatar_url, setAvatar_url] = useState("");
   const [age, setAge] = useState(0);
-  const [interests, setInterests] = useState("")
-
+  const [interests, setInterests] = useState("");
+  const [error, setError] = useState("");
+  const [emailError, setEmailError] = useState("")
+  const [passwordError, setPasswordError] = useState("")
+  const [usernameError, setUsernameError] = useState("")
+  const [firstNameError, setFirstNameError] = useState("")
+  const [lastNameError, setLastNameError] = useState("")
+  const [ageError, setAgeError] = useState()
+  const [interestsError, setInterestsError] = useState("")
 
   const signUp = async () => {
-    setIsLoading(true);
-    try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const userCreated = await postUser(username, first_name, last_name, +age, avatar_url, interests)
-      console.log('regscreen '+userCreated)
-    
-      alert("Check your email");
-    } catch (error) {
-      alert("SignUp failed: " + error.message);
-    } finally {
-      setIsLoading(false);
+    setEmailError("")
+    setPasswordError("")
+    setUsernameError("")
+    setFirstNameError("")
+    setLastNameError("")
+    setAgeError("")
+    setInterestsError("")
+    if (email.length === 0) {
+      setEmailError("email cannot be blank")
+    } else if (!password) {
+      setPasswordError("password needs to be at least 6 digits")
+    } else if (username.length === 0) {
+      setUsernameError("username cannot be blank");
+    } else if (first_name.length === 0) {
+      setFirstNameError("first name cannot be blank");
+    } else if (last_name.length === 0) {
+      setLastNameError("last name cannot be blank");
+    } else if (!age) {
+      setAgeError("age cannot be blank")
+    } else if (interests.length === 0) {
+      setInterestsError("interests cannot be blank");
+    } else {
+      try {
+        setIsLoading(true);
+          const response = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+          );
+          const userCreated = await postUser(
+            username,
+            first_name,
+            last_name,
+            +age,
+            avatar_url,
+            interests
+          );
+          console.log("regscreen " + userCreated);
+  
+          alert("Check your email");
+        }
+        catch(err) {
+        alert("SignUp failed: " + Error);
+      } finally {
+        setIsLoading(false);
+      }
     }
+   
   };
 
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: "center" }}>
-        <ScrollView>
-      <View style={{ paddingHorizontal: 25 }}>
-        <View style={{ alignItems: "center" }}>
-          <Image
-            source={require("../assets/images/badminton.jpg")}
-            style={{ width: "120%" }}
-          />
-        </View>
-        <Text
-          style={{
-            fontSize: 28,
-            fontWeight: "500",
-            color: "#333",
-            marginBottom: 30,
-          }}
-        >
-          Register
-        </Text>
-
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: 30,
-          }}
-        >
-          <TouchableOpacity
-            onPress={() => {}}
+      <ScrollView>
+        <View style={{ paddingHorizontal: 25 }}>
+          <View style={{ alignItems: "center" }}>
+            <Image
+              source={require("../assets/images/badminton.jpg")}
+              style={{ width: "120%" }}
+            />
+          </View>
+          <Text
             style={{
-              borderColor: "#ddd",
-              borderWidth: 2,
-              borderRadius: 10,
-              paddingHorizontal: 30,
-              paddingVertical: 10,
+              fontSize: 28,
+              fontWeight: "500",
+              color: "#333",
+              marginBottom: 30,
             }}
           >
-            <Image
-              source={require("../assets/images/google-icon.png")}
-              style={{ width: 24, height: 24 }}
-            />
-          </TouchableOpacity>
+            Register
+          </Text>
 
-          <TouchableOpacity
-            onPress={() => {}}
+          <View
             style={{
-              borderColor: "#ddd",
-              borderWidth: 2,
-              borderRadius: 10,
-              paddingHorizontal: 30,
-              paddingVertical: 10,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: 30,
             }}
           >
-            <Image
-              source={require("../assets/images/facebook-icon.png")}
-              style={{ width: 24, height: 24 }}
-            />
-          </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                borderColor: "#ddd",
+                borderWidth: 2,
+                borderRadius: 10,
+                paddingHorizontal: 30,
+                paddingVertical: 10,
+              }}
+            >
+              <Image
+                source={require("../assets/images/google-icon.png")}
+                style={{ width: 24, height: 24 }}
+              />
+            </TouchableOpacity>
 
-          <TouchableOpacity
-            onPress={() => {}}
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                borderColor: "#ddd",
+                borderWidth: 2,
+                borderRadius: 10,
+                paddingHorizontal: 30,
+                paddingVertical: 10,
+              }}
+            >
+              <Image
+                source={require("../assets/images/facebook-icon.png")}
+                style={{ width: 24, height: 24 }}
+              />
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => {}}
+              style={{
+                borderColor: "#ddd",
+                borderWidth: 2,
+                borderRadius: 10,
+                paddingHorizontal: 30,
+                paddingVertical: 10,
+              }}
+            >
+              <Image
+                source={require("../assets/images/twitter-icon.png")}
+                style={{ width: 24, height: 24 }}
+              />
+            </TouchableOpacity>
+          </View>
+          
+          <View
             style={{
-              borderColor: "#ddd",
-              borderWidth: 2,
-              borderRadius: 10,
-              paddingHorizontal: 30,
-              paddingVertical: 10,
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
             }}
           >
-            <Image
-              source={require("../assets/images/twitter-icon.png")}
-              style={{ width: 24, height: 24 }}
+            {
+              <MaterialIcons
+                name="alternate-email"
+                size={20}
+                color="#666"
+                style={{ marginRight: 5 }}
+              />
+            }
+            <TextInput
+              value={email}
+              style={{ flex: 1 }}
+              placeholder="email-address"
+              keyboardType="Email"
+              autoCapitalize="none"
+              onChangeText={(text) => setEmail(text)}
             />
-          </TouchableOpacity>
-        </View>
-
-        <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}
-        >
-          {
-            <MaterialIcons
-              name="alternate-email"
+             {{emailError} && <Text style={{color: "red"}}>{emailError}</Text>}
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
+            }}
+          >
+            <AntDesign
+              name="lock"
               size={20}
               color="#666"
               style={{ marginRight: 5 }}
             />
-          }
-          <TextInput
-            value={email}
-            style={{ flex: 1 }}
-            placeholder="email-address"
-            keyboardType="Email"
-            autoCapitalize="none"
-            onChangeText={(text) => setEmail(text)}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}
-        >
-          <AntDesign
-            name="lock"
-            size={20}
-            color="#666"
-            style={{ marginRight: 5 }}
-          />
 
-          <TextInput
-            style={{ flex: 1 }}
-            secureTextEntry={true}
-            value={password}
-            placeholder={"password"}
-            autoCapitalize="none"
-            onChangeText={(text) => setPassword(text)}
-            inputType="password"
-          />
-          </View>
-          <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}>
-          {
-            <Feather
-              name="user"
-              size={20}
-              color="#666"
-              style={{ marginRight: 5 }}
+            <TextInput
+              style={{ flex: 1 }}
+              secureTextEntry={true}
+              value={password}
+              placeholder={"password"}
+              autoCapitalize="none"
+              onChangeText={(text) => setPassword(text)}
+              inputType="password"
             />
-          }
-          <TextInput
-            value={username}
-            style={{ flex: 1 }}
-            placeholder="username"
-            keyboardType="username"
-            autoCapitalize="none"
-            onChangeText={(text) => setUsername(text)}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}
-        >
-          <TextInput
-            style={{ flex: 1 }}
-            value={first_name}
-            placeholder={"first-name"}
-            onChangeText={(text) => setFirst_name(text)}
-          />
+             {{passwordError} && <Text style={{color: "red"}}>{passwordError}</Text>}
           </View>
           <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}>
-          <TextInput
-            value={last_name}
-            style={{ flex: 1 }}
-            placeholder="last-name"
-            onChangeText={(text) => setLast_name(text)}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}
-        >
-          <TextInput
-            style={{ flex: 1 }}
-            value={avatar_url}
-            placeholder={"avatarURL"}
-            onChangeText={(text) => setAvatar_url(text)}
-          />
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
+            }}
+          >
+            {
+              <Feather
+                name="user"
+                size={20}
+                color="#666"
+                style={{ marginRight: 5 }}
+              />
+            }
+            <TextInput
+              value={username}
+              style={{ flex: 1 }}
+              placeholder="username"
+              keyboardType="username"
+              autoCapitalize="none"
+              onChangeText={(text) => setUsername(text)}
+            />
+           {{usernameError} && <Text style={{color: "red"}}>{usernameError}</Text>}
           </View>
           <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}>
-          <TextInput
-            style={{ flex: 1 }}
-            value={age}
-            placeholder={"age"}
-            onChangeText={(text) => setAge(text)}
-          />
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
+            }}
+          >
+            <TextInput
+              style={{ flex: 1 }}
+              value={first_name}
+              placeholder={"first-name"}
+              onChangeText={(text) => setFirst_name(text)}
+            />
+           {{firstNameError} && <Text style={{color: "red"}}>{firstNameError}</Text>}
           </View>
           <View
-          style={{
-            flexDirection: "row",
-            borderBottomColor: "#ccc",
-            borderBottomWidth: 1,
-            paddingBottom: 8,
-            marginBottom: 25,
-          }}>
-          <TextInput
-            style={{ flex: 1 }}
-            value={interests}
-            placeholder={"interests"}
-            onChangeText={(text) => setInterests(text)}
-          />
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
+            }}
+          >
+            <TextInput
+              value={last_name}
+              style={{ flex: 1 }}
+              placeholder="last-name"
+              onChangeText={(text) => setLast_name(text)}
+            />
+           {{lastNameError} && <Text style={{color: "red"}}>{lastNameError}</Text>}
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
+            }}
+          >
+            <TextInput
+              style={{ flex: 1 }}
+              value={avatar_url}
+              placeholder={"avatarURL"}
+              onChangeText={(text) => setAvatar_url(text)}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
+            }}
+          >
+            <TextInput
+              style={{ flex: 1 }}
+              value={age}
+              placeholder={"age"}
+              onChangeText={(text) => setAge(text)}
+            />
+            {{ageError} && <Text style={{color: "red"}}>{ageError}</Text>}
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+              paddingBottom: 8,
+              marginBottom: 25,
+            }}
+          >
+            <TextInput
+              style={{ flex: 1 }}
+              value={interests}
+              placeholder={"interests"}
+              onChangeText={(text) => setInterests(text)}
+            />
+            {{interestsError} && <Text style={{color: "red"}}>{interestsError}</Text>}
+          </View>
+          
+          {isLoading ? (
+            <ActivityIndicator size="large" color="#0000ff" />
+          ) : (
+            <>
+              <CustomButton label={"Sign Up"} onPress={signUp} />
+            </>
+          )}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <Text>Already registered? </Text>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Text style={{ color: "#AD40AF", fontWeight: "700" }}>Login</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        {isLoading ? (
-          <ActivityIndicator size="large" color="#0000ff" />
-        ) : (
-          <>
-            <CustomButton label={"Sign Up"} onPress={signUp} />
-          </>
-        )}
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center",
-          }}
-        >
-          <Text>Already registered? </Text>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Text style={{ color: "#AD40AF", fontWeight: "700" }}>Login</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
       </ScrollView>
     </SafeAreaView>
   );
