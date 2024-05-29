9e88cae292a9610071a166c8537034c1590f6c7b
ZoleeG <peter88.ujlaki@gmail.com>
2024-05-29 09:05:23 +0100

    fixed bug

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import AppStack from "./src/navigation/AppStack";
 import AuthStack from "./src/navigation/AuthStack";
-import {AuthProvider} from "./src/context/AuthContext";
 import { onAuthStateChanged } from "firebase/auth";
 import { FIREBASE_AUTH } from "./firebaseConfig";
 
 function App() {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
   useEffect(() => {
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      console.log(user)
-      setUser(user)
-    })
-  }, [])
-  
+      console.log(user);
+      setUser(user);
+    });
+  }, []);
+
   return (
-    <AuthProvider>
-      <NavigationContainer>
-        {user ? <AppStack /> : <AuthStack />}
-      </NavigationContainer>
-    </AuthProvider>
+    <NavigationContainer>
+      {user ? <AppStack /> : <AuthStack />}
+    </NavigationContainer>
   );
 }
 
